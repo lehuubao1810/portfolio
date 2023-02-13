e6f8e7ebbf3b092c5e23a6febe51331c0b587ebc
lehuubao1810 <lehuubao2909@gmail.com>
2023-02-13 21:38:33 +0700

    add project and change code CardProject

--- src/App.js
+++ src/App.js
@@ -7,12 +7,51 @@ function App() {
     alert('Downloading...');
   }
 
-
+  const projects = 
+  [
+    {
+        "title":"Project 1",
+        "image":"https://picsum.photos/500/300",
+        "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vitae ultricies ultricies, nisl nisl aliquam nisl, eget aliquam nisl nisl eu nisl.",
+        "skills": "React, NodeJS, MongoDB",
+        "github":"https://github.com/lehuubao1810",
+        "preview":"#",
+        "type": "web"
+    },
+    {
+        "title":"MATH BIN",
+        "image":"https://lehuubao1810.github.io/dataquiz/mathbin.png",
+        "description":"A mobile application helps students improve their math skills by quizzes and exercises.",
+        "skills": "React, NodeJS, MongoDB",
+        "github":"https://github.com/lehuubao1810/QuizApp",
+        "preview":"https://drive.google.com/file/d/1w_o6jFag7hOHpiWwKWTvRd0DmqnqmmD4/view?usp=share_link",
+        "type": "mobile"
+    },
+    {
+        "title":"Calculator App",
+        "image":"https://raw.githubusercontent.com/lehuubao1810/Calculator-App/main/design/desktop-design-theme-1.jpg",
+        "description":"A calculator app can perform basic arithmetic operations (add, subtract, multiply, divide) and have three themes",
+        "skills": "React, NodeJS, MongoDB",
+        "github":"https://github.com/lehuubao1810/Calculator-App",
+        "preview":"https://lehuubao1810.github.io/Calculator-App/",
+        "type": "web"
+    },
+    {
+        "title":"Rest Countries API",
+        "image":"https://raw.githubusercontent.com/lehuubao1810/countriesAPI/main/design/desktop-design-home-dark.jpg",
+        "description":"A web application that allows users to search for countries using the REST Countries API and filter them by region.",
+        "skills": "React, Rest API",
+        "github":"https://github.com/lehuubao1810/countriesAPI",
+        "preview":"https://lhbcountriesapi.vercel.app/",
+        "type": "web"
+    }
+  ]
 
   const skills = [ 
     'ReactJS', 'HTML', 'CSS', 'JavaScript',
-     'TypeScript', 'TailwindCSS', 'Sass',
-    'React Native', 'Git', 'Redux', 'NextJS',
+     'TypeScript', 'TailwindCSS', 'Sass/SCSS',
+    'React Native', 'Git', 'Redux', 'NextJS', 
+    'Rest API'
     ]
   return (
     <div className="App">
@@ -41,42 +80,9 @@ function App() {
         <hr className='line'></hr>
         <h1>Projects</h1>
         <div className="cards">
-          <CardProject 
-            title="Project 1"
-            image="https://picsum.photos/500/300"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vitae ultricies ultricies, nisl nisl aliquam nisl, eget aliquam nisl nisl eu nisl."
-            skills= "React, NodeJS, MongoDB"
-            github="https://github.com/lehuubao1810"
-            preview="#"
-            type= "web"
-          />
-          <CardProject 
-            title="MATH BIN"
-            image="https://lehuubao1810.github.io/dataquiz/mathbin.png"
-            description="A android application helps students improve their math skills by quizzes and exercises."
-            skills= "React Native, JSON Server"
-            github="https://github.com/lehuubao1810/QuizApp"
-            // preview="https://expo.dev/artifacts/eas/envG2WyKpFTJZ5hEBA3U5v.apk"
-            preview="https://drive.google.com/file/d/1w_o6jFag7hOHpiWwKWTvRd0DmqnqmmD4/view?usp=share_link"
-          />
-          <CardProject 
-            title="Calculator App" 
-            image="https://raw.githubusercontent.com/lehuubao1810/Calculator-App/main/design/desktop-design-theme-1.jpg"
-            description=" A calculator app can perform basic arithmetic operations (add, subtract, multiply, divide) and have three themes."
-            skills= "React, NodeJS, MongoDB"
-            github="https://github.com/lehuubao1810/Calculator-App"
-            preview="https://lehuubao1810.github.io/Calculator-App/"
-            type= "web"
-          />
-          <CardProject 
-            title="Project 4"
-            image="https://picsum.photos/500/300"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vitae ultricies ultricies, nisl nisl aliquam nisl, eget aliquam nisl nisl eu nisl."
-            skills= "React, NodeJS, MongoDB"
-            github="https://github.com/lehuubao1810"
-            preview="#"
-            type= "web"
-          />
+          {projects.map((project, index) => (
+            <CardProject key={index} project={project} />
+          ))}
         </div>
       </section>
       <section id='about'>
--- src/components/cardProject.jsx
+++ src/components/cardProject.jsx
@@ -1,14 +1,14 @@
 const CardProject = (props) => {
-    const skills = props.skills.split(", ");
-    const isWeb = props.type === "web";
+    const skills = props.project.skills.split(", ");
+    const isWeb = props.project.type === "web";
     return(
         <div className="card-project">
             <div className="card-project__image">
-                <img src={props.image} alt={props.title} />
+                <img src={props.project.image} alt={props.project.title} />
             </div>
             <div className="card-project__content">
-                <h3 className="card-project__title">{props.title}</h3>
-                <p className="card-project__description">{props.description}</p>
+                <h3 className="card-project__title">{props.project.title}</h3>
+                <p className="card-project__description">{props.project.description}</p>
                 <div className="card-project__skills">
                     {skills.map((skill, index) => {
                         return(
@@ -18,8 +18,8 @@ const CardProject = (props) => {
                     }
                 </div>
                 <div className="card-project__links">
-                    <a href={props.github}><i className="fa-brands fa-github"></i></a>
-                    <a href={props.preview}>
+                    <a href={props.project.github}><i className="fa-brands fa-github"></i></a>
+                    <a href={props.project.preview}>
                         {
                             isWeb ?
                             <i className="fa-solid fa-up-right-from-square"></i> :
