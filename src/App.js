3e86aeffdd1b2b7e17e942e0586494b8d8456006
lhb1810 <lehuubao2909@gmail.com>
2023-03-15 16:00:21 +0700

    fix skills

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
     ]
 
   const skills = [
-    'ReactJS', 'HTML', 'CSS', 'JS', 'JavaScript',
+    'ReactJS', 'HTML', 'CSS', 'JavaScript',
     'TailwindCSS', 'Sass/SCSS', 'React Native',
     'Git', 'Redux', 'Firebase', 'Rest API'
   ]
