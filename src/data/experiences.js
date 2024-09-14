ff543c3419a7f94a08a27e00c7c2800f440b7ee8
lehuubao1810 <lehuubao2909@gmail.com>
2024-09-14 21:41:16 +0700

    feat: Add FrontEnd Developer experience at Goku Agency

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -1,5 +1,15 @@
 export const experiences = [
   {
+    title: "FrontEnd Developer",
+    company: "Goku Agency",
+    time: "Aug 2024 - Now",
+    description: [
+      "Develop 360-degree marketing website for clients.",
+      "Design and develop user interfaces for web applications.",
+      "Optimize application for maximum speed and scalability.",
+    ],
+  },
+  {
     title: "React Native Intern",
     company: "SGOD",
     time: "Apr 2024 - Jun 2024",
